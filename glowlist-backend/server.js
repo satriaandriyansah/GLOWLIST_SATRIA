e922cab6298de5dd72d3a476b63620638d4c0495
satria <satriaandriyansah88@gmail.com>
2026-08-11 14:19:24 +0700

    create

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const app = express ();
 const PORT = 3001;
+const mysql = require ('mysql2');
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database:'glowlist_db'
+});
+db.connect(err => {
+    if(err){
+        console.error('Gagal konek ke database:',err);
+    } else {
+        console.log ('Berhahasil konek ke database Glowlist');
+    }
+});
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('Selamat Datang di Glowlist API sudah berjalan');
 });
-app.get("/coba", (req, res) => {
-    res.send('Selamat Datang di Glowlist coba sudah berjalan ');
+
+app.get("/produk", (req, res) => {
+    const sql = 'SELECT * FROM produk';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+    });
 });
+
+app.get("/kategori", (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+    });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server Glowlist jalan di http://localhost:${PORT}`);
