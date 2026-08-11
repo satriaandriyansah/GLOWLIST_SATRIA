444fba2fc389943048be86ac24573db9761d5040
satria <satriaandriyansah88@gmail.com>
2026-08-11 11:34:48 +0700

    Update server.js

--- glowlist-backend/server.js
+++ glowlist-backend/server.js
@@ -5,9 +5,12 @@ const PORT = 3001;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Selamat Datang di Glowlist 🔥🔥');
+    res.send('Selamat Datang di Glowlist API sudah berjalan');
+});
+app.get("/coba", (req, res) => {
+    res.send('Selamat Datang di Glowlist coba sudah berjalan ');
 });
 
 app.listen(PORT, () => {
     console.log(`Server Glowlist jalan di http://localhost:${PORT}`);
-});
+}); 
